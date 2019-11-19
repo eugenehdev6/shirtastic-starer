cac58148819374335fbf87f4bd2caaa81a6a2954
Eugene Havrylov <eugeneh@dev6.com>
2019-11-19 09:50:11 -0500

    .

--- src/templates/product-template.js
+++ src/templates/product-template.js
@@ -3,7 +3,6 @@ import ProductItem from "../components/product-item"
 import Layout from "../hoc/layout"
 
 const ProductTemplate = props => {
-  console.log("Function: ProductTemplate, props: ", props)
   return (
     <Layout>
       <ProductItem productItem={props.pageContext} />
