8e15da31eef0dc5f08fdc35900038f198b5d08e6
Eugene Havrylov <eugeneh@dev6.com>
2019-11-20 14:57:53 -0500

    .

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,7 +2,6 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import React from "react"
 import { StateProvider } from "./src/state/state"
 
-
 export const wrapRootElement = ({ element }) => {
   return <StateProvider>{element}</StateProvider>
 }
